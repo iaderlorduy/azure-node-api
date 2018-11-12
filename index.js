0d6190b64d67fc252dc405b49bbd3886bd75a35a
iaderlorduy <iaderlor@gmail.com>
2018-11-11 23:41:45 -0500

    init commit

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
-let express = require('express');
-let HttpStatus = require('http-status-codes');
+var http = require('http');
 
-let app = express();
+var server = http.createServer(function(request, response) {
 
-let gameRoute = require('./src/routes/game');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 
-let path = require('path');
-
-let bodyParser = require('body-parser');
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
-app.use(bodyParser.json());
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-app.use((req, res, next) => {
-  console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body);
-  next();
-})
-
-app.use(gameRoute);
-app.use(express.static('./public'));
-
-// Handler for 404 - Resource not found
-app.use((req, res, next) => {
-  res.status(HttpStatus.NOT_FOUND).send('Resource not found');
-  next();
-})
-
-// Handler for 500 - 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.sendFile(path.join(__dirname, '../public/Error.html'))
-})
-
-
-
-const PORT = process.env.PORT || 1337
-app.listen(PORT, () => {
-  console.info(`Server has started on ${PORT}`)
-})
+console.log("Server running at http://localhost:%d", port);
