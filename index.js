068b974d991aa8ccc93bc48c12fdc4acef1c10af
iaderlorduy <iaderlor@gmail.com>
2018-11-12 00:05:18 -0500

    add 'use strict';

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+'use strict';
 var express = require('express');
-var http = require('http');
+var HttpStatus = require('http-status-codes');
 
-var server = http.createServer(function(request, response) {
+var app = express();
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+var gameRoute = require('./src/routes/game');
 
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.use(bodyParser.json());
 
-console.log("Server running at http://localhost:%d", port);
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body);
+  next();
+})
+
+app.use(gameRoute);
+app.use(express.static('./public'));
+
+// Handler for 404 - Resource not found
+app.use((req, res, next) => {
+  res.status(HttpStatus.NOT_FOUND).send('Resource not found');
+  next();
+})
+
+// Handler for 500 - 
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.sendFile(path.join(__dirname, '../public/Error.html'))
+})
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.info(`Server has started on ${PORT}`)
+})
---
+++ src/models/game.model.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var server = 'ds231242.mlab.com:31242';
+var database = 'god';
+var user = 'root';
+var password = 'abc123';
+
+mongoose.connect(
+  `mongodb://${user}:${password}@${server}/${database}`,
+  {
+    useNewUrlParser: true
+  },
+  (err) => {
+    if (err) console.log("test")
+  }
+);
+
+var GameSchema = new mongoose.Schema({
+  player1Name: String,
+  player2Name: String,
+  scorePlayer1: Number,
+  scorePlayer2: Number,
+  playerWinner: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Game', GameSchema);
