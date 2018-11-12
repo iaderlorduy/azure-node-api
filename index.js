0452bb60b431f7cc6dddff6c1e4cce93a80e5c81
iaderlorduy <iaderlor@gmail.com>
2018-11-11 23:53:03 -0500

    default index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let express = require('express');
-let HttpStatus = require('http-status-codes');
+var http = require('http');
 
-let app = express();
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
 var port = process.env.PORT || 1337;
+server.listen(port);
 
-app.get('/', (req, res) => {
-  return res.send('hola a todos');
-})
-
-app.listen(PORT, () => {
-  console.info(`Server has started on ${PORT}`)
-})
+console.log("Server running at http://localhost:%d", port);
